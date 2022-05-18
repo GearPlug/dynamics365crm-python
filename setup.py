7aa52d288dbcd27e132f1556b0a39c660ebc2abe
sgiraldo <sgiraldo@gearplug.io>
2022-05-18 11:13:11 -0500

    feat: update library version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="dynamics365crm-python",
-    version="0.1.2",
+    version="0.2.0",
     description="API wrapper for Dynamics365CRM written in Python",
     long_description=read("README.md"),
     url="https://github.com/GearPlug/dynamics365crm-python",
